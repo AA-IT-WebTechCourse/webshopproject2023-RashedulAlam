45bc14c6b24fb9a8164473fef0a73b75c41b86e3
Rashedul Alam <rashedulalam.iit.du@gmail.com>
2024-03-29 01:02:18 +0200

    added product card

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,10 @@
 import Register from "@/components/account/register";
+import ProductCard from "@/components/product/productCard";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-       <Register /> 
+       <ProductCard /> 
     </main>
   );
 }
---
+++ frontend/src/components/product/productCard.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { IProductCardProps } from "./product.d";
+
+const ProductCard = ({
+  categories,
+  description,
+  price,
+  tag,
+  title,
+  photo,
+}: IProductCardProps) => {
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+          alt="Shoes"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          Shoes!
+          <div className="badge badge-secondary">NEW</div>
+        </h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div className="card-actions justify-end">
+          <div className="badge badge-outline">Fashion</div>
+          <div className="badge badge-outline">Products</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
