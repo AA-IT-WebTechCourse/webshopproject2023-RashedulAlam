b680d4d997831a6f925d12ce21753d3dda0aa444
Rashedul Alam <rashedulalam.iit.du@gmail.com>
2024-03-28 23:48:09 +0200

    added login and register page routing

---
+++ frontend/src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+import Register from "@/components/account/register";
+
+export default function SignUpPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Register />
+    </main>
+  );
+}
