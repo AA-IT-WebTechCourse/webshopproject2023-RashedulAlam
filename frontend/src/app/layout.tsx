1ad291b6900ce1c729dbcb0b4968b35ab119875d
Rashedul Alam <rashedulalam.iit.du@gmail.com>
2024-09-28 18:44:17 +0300

    updated layout style

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         <AuthenticationProvider>
           <CartProvider>
             <Navbar />
-            {children}
+            <div className="max-w-screen-xl mx-auto">{children}</div>
             <Footer />
           </CartProvider>
           <ToastContainer
